75d2afe6831b2a8b48cc8a405ceb126ae7bb0e1b
Andrew Lock <andrew.john.lock@gmail.com>
2025-09-09 16:07:42 +1000

    Add first install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install dependencies
+sudo apt-get update
+sudo apt-get install -y build-essential cmake libssl-dev libboost-all-dev
+openjdk-11-jdk
+
+# Download and set up Certus Mini Manager
+curl -LO https://www.advancednavigation.com/downloads/latest/certus-mini-d/CertusMiniManager-7.5.zip
+unzip CertusMiniManager-7.5.zip -d certus_manager
+
+# Set up Python packages
+pip install -r requirements.txt
+
+# Compile Certus C++ SDK interface
+cd cpp/certus
+make
+cd ../../
+

e8d5c9cac19fb1f2d75416a8b036eeea4f495402
Andrew Lock <andrew.john.lock@gmail.com>
2025-11-04 16:50:21 +1000

    Change TUI default config path

--- tui/__main__.py
+++ tui/__main__.py
@@ -2,11 +2,17 @@ import argparse
 from pathlib import Path
 from .tui import HealthApp
 
+
 # -------------- entrypoint --------------
 def main():
-    filepath = Path(__file__).parent.resolve()
     p = argparse.ArgumentParser(description="Health Monitoring TUI Application")
-    p.add_argument("--config", type=Path, required=False, default=Path(filepath, "$HOME/obs-config/config.yaml"), help="Path to the configuration file")
+    p.add_argument(
+        "--config",
+        type=Path,
+        required=False,
+        default=Path(Path.home(), "obs-default-config.yaml"),
+        help="Path to the configuration file",
+    )
     args = p.parse_args()
     HealthApp(args.config).run()
 

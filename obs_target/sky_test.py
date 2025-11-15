92b7e987d07291ceb82fc80b424903d06994b4ce
Andrew Lock <andrew.john.lock@gmail.com>
2025-11-15 22:39:10 +1000

    Fix sky script

---
+++ obs_target/sky_test.py
@@ -0,0 +1,138 @@
+from astropy.time import Time
+from astropy.coordinates import solar_system_ephemeris, EarthLocation
+from astropy.coordinates import get_body
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+from astropy import units as u
+from numpy.typing import ArrayLike, NDArray
+import numpy as np
+
+from obs_target.target import Target
+
+import astrix as at
+from astrix.spatial import Rotation
+
+BRIGHT_STARS = {
+    "Sirius": ("06h45m08.917s", "-16d42m58.02s"),
+    "Canopus": ("06h23m57.109s", "-52d41m44.38s"),
+    "Arcturus": ("14h15m39.672s", "+19d10m56.67s"),
+    "Vega": ("18h36m56.336s", "+38d47m01.28s"),
+    "Capella": ("05h16m41.358s", "+45d59m52.77s"),
+    "Rigel": ("05h14m32.272s", "-08d12m05.90s"),
+    "Procyon": ("07h39m18.119s", "+05d13m29.96s"),
+    "Achernar": ("01h37m42.845s", "-57d14m12.33s"),
+    "Betelgeuse": ("05h55m10.305s", "+07d24m25.43s"),
+    "Hadar": ("14h03m49.405s", "-60d22m22.72s"),
+    "Altair": ("19h50m47.005s", "+08d52m05.96s"),
+    "Aldebaran": ("04h35m55.239s", "+16d30m33.49s"),
+    "Antares": ("16h29m24.459s", "-26d25m55.21s"),
+    "Spica": ("13h25m11.579s", "-11d09m40.75s"),
+    "Fomalhaut": ("22h57m39.046s", "-29d37m20.05s"),
+}
+PLANETS = [
+    "earth",
+    "sun",
+    "moon",
+    "mercury",
+    "venus",
+    "earth-moon-barycenter",
+    "mars",
+    "jupiter",
+    "saturn",
+    "uranus",
+    "neptune",
+]
+
+
+class SkyTarget(Target):
+    _name: str
+    _point: at.Point
+    _cam: at.FixedZoomCamera | None
+
+    def __init__(self, name: str, point: at.Point, cam: at.FixedZoomCamera) -> None:
+        self._name = name
+        self._point = point
+        self._cam = cam
+        self._frame_ned = at.spatial.frame.ned_frame(self._point)
+
+    def check_time_bounds(self, t_unix: float) -> tuple[bool, bool]:
+        return True, True
+
+    def get_planet(self, name: str, t_unix: float) -> tuple[float, float]:
+        lat, lon, height = self._point.geodet
+        t = Time(t_unix, format="unix")
+        loc = EarthLocation(lat=lat * u.deg, lon=lon * u.deg, height=height * u.meter)
+
+        with solar_system_ephemeris.set("jpl"):
+            planet = get_body(name, t, loc)
+
+        altazframe = AltAz(obstime=t, location=loc, pressure=0)
+        planetaz = planet.transform_to(altazframe)
+        az = planetaz.az.degree
+        el = planetaz.alt.degree
+        return az, el
+
+    def get_star(self, name: str, t_unix: float) -> tuple[float, float]:
+        lat, lon, height = self._point.geodet
+        time = Time(t_unix, format="unix")
+        loc = EarthLocation(lat=lat * u.deg, lon=lon * u.deg, height=height * u.meter)
+        ra, dec = BRIGHT_STARS[name]
+        star_icrs = SkyCoord(ra=ra, dec=dec, unit=(u.hourangle, u.deg), frame="icrs")
+        altaz = star_icrs.transform_to(AltAz(obstime=time, location=loc))
+        az = altaz.az.degree
+        el = altaz.alt.degree
+        return az, el
+
+    def _get_az_el(self, name: str, t_unix: float) -> tuple[float, float]:
+        """Get azimuth and elevation of a sky target (star or planet) from a given time and location.
+        Args:
+            name (str): Name of the sky target (star or planet).
+            t_unix (float): Unix time to get the azimuth and elevation.
+        Returns:
+            az (float): Azimuth in degrees.
+            el (float): Elevation in degrees.
+        """  # Get current location from target path
+
+        if name in BRIGHT_STARS:
+            return self.get_star(name, t_unix)
+        elif name in PLANETS:
+            return self.get_planet(name, t_unix)
+        else:
+            raise ValueError(f"Unknown sky target name: {name}")
+
+    def get_head_pitch(self, t_unix: float) -> NDArray:
+        az, el = self._get_az_el(self._name, t_unix)
+        heading = az
+        pitch = el
+        return np.array([heading, pitch])
+
+    def project_from_ned_angles(
+        self, euler: ArrayLike, t_unix: float
+    ) -> tuple[ArrayLike, ArrayLike]:
+        if self._cam is None:
+            raise RuntimeError("Camera is not defined for this target.")
+        hp = self.get_head_pitch(t_unix)
+        rot = Rotation.from_euler("ZYX", np.array(euler).reshape(1, -1), degrees=True)
+        frame = at.Frame(rot, ref_frame=self._frame_ned)
+        ray = at.Ray.from_az_el(
+            az_el=hp,
+            frame=frame,
+            time=at.Time(t_unix),
+        )
+        uv = ray.project_to_cam(self._cam)
+        return uv.uv, uv.uv
+
+    def project_from_ecef_angles(
+        self, euler: ArrayLike, t_unix: float
+    ) -> tuple[ArrayLike, ArrayLike]:
+        if self._cam is None:
+            raise RuntimeError("Camera is not defined for this target.")
+        hp = self.get_head_pitch(t_unix)
+        rot = Rotation.from_euler("ZYX", np.array(euler).reshape(1, -1), degrees=True)
+        frame = at.Frame(rot)
+        ray = at.Ray.from_az_el(
+            az_el=hp,
+            frame=frame,
+            time=at.Time(t_unix),
+        )
+        uv = ray.project_to_cam(self._cam)
+        return uv.uv, uv.uv
--- run_scripts/main.py
+++ run_scripts/main.py
@@ -1,15 +1,23 @@
+import time
+
 from obs_cameras.alvium import Alvium811
 from obs_cameras.base import CameraStream
 from obs_display.display import Display
-from obs_overlay.overlay import PathOverlay, read_varda_traj
+from obs_target.target import PathTarget
+from obs_target.sky_test import SkyTarget
+from obs_target.parse import read_varda_traj
+from obs_controller.controller import GimbalController
 
 from obs_certus.monitor import CertusMonitor
 from obs_encoders.monitor import EncoderMonitor
+from obs_utils.context import Context, State
 
 import astrix as at
 
 
 def main():
+
+    # Create objects from data
     path_nom = read_varda_traj(
         "~/varda-w4/planning/data/W4_Nominal_ECEF.csv",
         test_time_adjustment=True,
@@ -19,14 +27,25 @@ def main():
         (2848, 2848), (2.74 * 1e-3 * 2848, 2.74 * 1e-3 * 2848), 21
     )
 
-    alv811 = Alvium811()
-    imu = CertusMonitor(az_rot_deg=180, pc_time=1)
-    encoders = EncoderMonitor()
-    stream = CameraStream("Test_alvium", alv811, "~/test_image_data")
-    overlay = PathOverlay(pt_GS2, path_nom, alv811_25_mod)
-    display = Display("test_cam", stream, imu, encoders, overlay)
+    # Instantiate state and monitors
+    # state = State()
+    # imu_monitor = CertusMonitor(sink=state.set_imu_state)
+    # controller = Controller(sink=state.set_controller_state)
+    # context = Context(
+    #     imu_monitor=imu_monitor,
+    #     controller=controller,
+    # )
 
-    display.run()
+    target = SkyTarget("Canopus", pt_GS2, alv811_25_mod)
+    head, pitch = target.get_head_pitch(time.time())
+    print("Head:", head)
+    print("Pitch:", pitch)
+
+
+
+
+
+
 
 
 if __name__ == "__main__":

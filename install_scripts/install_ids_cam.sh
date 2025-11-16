ae48871017cee01422623cd6939492445f3d61e9
Andrew Lock <andrew.john.lock@gmail.com>
2025-11-16 18:36:35 +1000

    Add ids install script

---
+++ install_scripts/install_ids_cam.sh
@@ -0,0 +1,24 @@
+
+
+CWD="$(pwd)"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+
+mkdir -p "$SCRIPT_DIR"/../obs_cameras/assets
+cd "$SCRIPT_DIR"/../obs_cameras/assets
+
+if [ ! -d "/opt/ids-peak_2.18.1.0-132_amd64" ]; then
+    echo "IDS Peak SDK not found. Installing..."
+    curl -LO https://en.ids-imaging.com/files/downloads/ids-peak/software/linux-desktop/ids-peak_2.18.1.0-132_amd64.tgz
+    tar -xvzf ids-peak_2.18.1.0-132_amd64.tgz
+    sudo mv ids-peak_2.18.1.0-132_amd64 /opt
+else
+    echo "IDS Peak SDK already installed."
+fi
+
+# Add LD_LIBRARY_PATH to .bashrc if not already present
+if ! grep -q 'LD_LIBRARY_PATH=/opt/ids-peak_2.18.1.0-132_amd64/lib' ~/.bashrc; then
+    echo 'export LD_LIBRARY_PATH=/opt/ids-peak_2.18.1.0-132_amd64/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH' >> ~/.bashrc
+fi
+
+
